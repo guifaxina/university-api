8ff7372178122ac2ee3005f6b414b55876e886da
Guilherme Faxina <gamerstarbr7@hotmail.com>
2022-12-21 14:15:20 -0300

    feat: express-ts server

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,9 @@
 import express from "express";
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => console.log(`Server running http://localhost:${PORT}`));
