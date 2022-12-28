3ddf1856ebec1cac228fbf87c5a89569da65cb58
Guilherme Faxina <gamerstarbr7@hotmail.com>
2022-12-28 12:27:25 -0300

    refactor(model): correct types and add require

--- src/models/university-model.ts
+++ src/models/university-model.ts
@@ -1,17 +1,36 @@
 import mongoose from "mongoose";
 
 const universitySchema = new mongoose.Schema({
-  alpha_two_code: String,
+  alpha_two_code: { 
+    type: String, 
+    require: true
+  }, 
   
-  name: String,
+  name: { 
+    type: String, 
+    require: true
+  },
   
-  web_pages: [{ type: String }],
+  web_pages: [{ 
+    type: String, 
+    required: true 
+  }],
   
-  "state-province": String || null,
+  "state-province": { 
+    type: String || null, 
+    require: true
+  },
   
-  domains: [{ type: String }],
+  domains: { 
+    type: String || null, 
+    require: true
+  },
   
-  country: { type: String, lowercase: true }
+  country: { 
+    type: String,
+    require: true, 
+    lowercase: true 
+  }
 });
 
 const UniversityModel = mongoose.model("University", universitySchema);
