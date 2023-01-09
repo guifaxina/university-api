9aa5daaec6c695cafed2a1c90e62d10e25f223e9
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-01-09 15:22:56 -0300

    feat: add delete university route

--- src/routes/universities-route.ts
+++ src/routes/universities-route.ts
@@ -6,13 +6,11 @@ const router = Router();
 
 router.get("/universities", UniversityController.listUniversities);
 router.get("/universities/:id", UniversityController.listUniversityById);
+
 router.post("/universities", UniversityController.registerUniversity);
+
 router.put("/universities/:id", UniversityController.updateUniversity);
 
-router.delete("/delete", async (req, res) => {
-  const response = await UniversityModel.deleteMany({ alpha_two_code: "BR" });
-
-  res.status(200).json({ message: "deleted successfully", data: response });
-});
+router.delete("/universities/:id", UniversityController.deleteUniversity);
 
 export default router;
