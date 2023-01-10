1d0258ee9892ad3e3937f07925457591c0c60bbb
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-01-10 13:36:24 -0300

    feat: add proper error handling to class  methods

--- src/controllers/universities-controller.ts
+++ src/controllers/universities-controller.ts
@@ -3,24 +3,38 @@ import IUniversity from "../interfaces/university";
 import UniversityModel from "../models/university-model";
 
 class UniversityController {
-  public async listUniversities(req: Request, res: Response): Promise<Response> {
+  public async listUniversities(req: Request, res: Response): Promise<Response | void> {
     const { search } = req.query;
     const projection = "_id name country state-province";
 
     if (!search) {
-      const findAllUniversities = await UniversityModel.find({}, projection);
-      return res.status(200).json({ status: "success", message: "Successfully retrieved all universities", data: findAllUniversities });
+      try {
+        const findAllUniversities = await UniversityModel.find({}, projection);
+        if (findAllUniversities.length !== 0)
+          return res.status(200).json({ status: "success", message: "Successfully retrieved all universities", data: findAllUniversities });
+        
+        else 
+          return res.status(404).json({ status: "error", message: "No universities were found." });
+      } catch (error) {
+        if (error instanceof Error) 
+          return res.send(500).json({ status: "error", name: error.name, message: error.message });
+      }
     } 
     
-    const findUniversitiesFrom = await UniversityModel.find({ country: search }, projection);
-    if (findUniversitiesFrom.length !== 0) 
-      return res.status(200).json({ status: "success", message: `Successfully found all universities from ${search}.`, data: findUniversitiesFrom });
+    try {
+      const findUniversitiesFrom = await UniversityModel.find({ country: search }, projection);
+      if (findUniversitiesFrom.length !== 0) 
+        return res.status(200).json({ status: "success", message: `Successfully found all universities from ${search}.`, data: findUniversitiesFrom });
     
-    else 
-      return res.status(404).json({ status: "failed", message: "Error, country not found." });
+      else 
+        return res.status(404).json({ status: "failed", message: "Error, country not found." });
+    } catch (error) {
+      if (error instanceof Error) 
+        return res.send(500).json({ status: "error", name: error.name, message: error.message });
+    }
   }
 
-  public async listUniversityById(req: Request, res: Response): Promise<Response> {
+  public async listUniversityById(req: Request, res: Response): Promise<Response | void> {
     const { id } = req.params;
     
     try {
@@ -32,23 +46,30 @@ class UniversityController {
         return res.status(404).json({ status: "error", message: "University not found."});
     } catch (error) {
       if (error instanceof Error) {
-        return res.status(400).json({ status: "Error", name: error.name, message: error.message, description: "Expected a valid id." });
+        if (error.name === "CastError")
+          return res.status(400).json({ status: "Error", name: error.name, message: error.message, description: "Expected a valid id." });
+        else 
+          return res.status(500).json({ status: "error", message: "Oops something went wrong!"});
       }
     }
-    return res.status(500).json({ status: "error", message: "Oops something went wrong!"});
   }
 
-  public async registerUniversity(req: Request, res: Response): Promise<Response> {
+  public async registerUniversity(req: Request, res: Response): Promise<Response | void> {
     const insertUnis = new InsertUnis();
     const university: IUniversity = req.body.data;
-    const response = await insertUnis.insert(university);
     
-    return res.status(201).json({ status: "success", message: "Successfully registered new university.", data: response });
+    try {
+      const response = await insertUnis.insert(university);
+      if (response)
+        return res.status(201).json({ status: "success", message: "Successfully registered new university.", data: response });
+    } catch (error) {
+      if (error instanceof Error)
+        return res.status(500).json({ status: "error", name: error.name, message: error.message });
+    }
   }
 
-  public async updateUniversity(req: Request, res: Response) {
+  public async updateUniversity(req: Request, res: Response): Promise<Response | void> {
     const { id } = req.params;
-    
     const { web_pages, name, domains } = req.body;
     
     try {
@@ -69,18 +90,21 @@ class UniversityController {
     }
   }
 
-  public async deleteUniversity(req: Request, res: Response) {
+  public async deleteUniversity(req: Request, res: Response): Promise<Response | void> {
     const { id } = req.params;
 
-    const response = await UniversityModel.findByIdAndDelete(id, { new: true });
+    try {
+      const response = await UniversityModel.findByIdAndDelete(id, { new: true });
     
-    if (response) 
-      return res.status(200).json({ status: "success", message: "Successfully deleted University.", data: response });
+      if (response) 
+        return res.status(200).json({ status: "success", message: "Successfully deleted University.", data: response });
     
-    if (response === null)
-      return res.status(404).json({ status: "error", message: "University does not exist."});
-
-    return res.status(500).json({ status: "error", message: "Oops something went wrong." });
+      if (response === null)
+        return res.status(404).json({ status: "error", message: "University does not exist."});
+    } catch (error) {
+      if (error instanceof Error)
+        return res.status(500).json({ status: "error", name: error.name, message: error.message });
+    }
   }
 }
 
