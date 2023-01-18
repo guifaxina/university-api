72f0fd2277c558ff7bc8092f85853d010bdf73cc
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-01-18 10:53:06 -0300

    feat: change mongo connection to environment var

--- src/databases/mongodb-settings.ts
+++ src/databases/mongodb-settings.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export default async function mongodbConnection () {
   try {
     mongoose.set("strictQuery", false);
-    await mongoose.connect("mongodb://localhost:27017/universities");
+    await mongoose.connect(`${process.env.MONGO_CONNECTION_URI}`);
     console.log("MongoDB connection established.");
   } catch (err) {
     console.error(err);
